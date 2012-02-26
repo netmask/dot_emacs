b3091f5bcffe5a9d43143de4b3f90dcefe605676
Jonathan Garay <jonathan@devmask.net>
2012-02-26 10:04:17 -0600

    my .emacs

---
+++ init.el
@@ -0,0 +1,118 @@
+;;; init.el --- Emacs Prelude: configuration entry point.
+;;
+;; Copyright (c) 2011 Bozhidar Batsov
+;;
+;; Author: Bozhidar Batsov <bozhidar.batsov@gmail.com>
+;; URL: http://www.emacswiki.org/cgi-bin/wiki/Prelude
+;; Version: 1.0.0
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; This file simply sets up the default load path and requires
+;; the various modules defined within Emacs Prelude.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+(require 'cl)
+
+(message "%s" "Emacs Prelude is powering up. Be patient, Master.")
+
+;; On OS X Emacs doesn't use the shell PATH if it's not started from
+;; the shell. If you're using homebrew modifying the PATH is essential.
+(if (eq system-type 'darwin)
+    (push "/usr/local/bin" exec-path))
+
+(defvar prelude-dir (file-name-directory load-file-name)
+  "The root dir of the Emacs Prelude distribution.")
+(defvar prelude-modules-dir (concat prelude-dir "modules/")
+  "This directory houses all of the built-in Prelude module. You should
+avoid modifying the configuration there.")
+(defvar prelude-vendor-dir (concat prelude-dir "vendor/")
+  "This directory house Emacs Lisp packages that are not yet available in
+ELPA (or Marmalade).")
+(defvar prelude-personal-dir (concat prelude-dir "personal/")
+  "Users of Emacs Prelude are encouraged to keep their personal configuration
+changes in this directory. All Emacs Lisp files there are loaded automatically
+by Prelude.")
+
+(add-to-list 'load-path prelude-modules-dir)
+(add-to-list 'load-path prelude-vendor-dir)
+(add-to-list 'load-path prelude-personal-dir)
+
+;; config changes made through the customize UI will be store here
+
+(setq custom-file (concat prelude-personal-dir "custom.el"))
+
+;; the core stuff
+(require 'prelude-packages)
+(require 'prelude-el-get)
+(require 'prelude-ui)
+(require 'prelude-core)
+(require 'prelude-editor)
+(require 'prelude-global-keybindings)
+
+;; programming & markup languages support
+(require 'prelude-programming)
+(require 'prelude-c)
+(require 'prelude-clojure)
+(require 'prelude-coffee)
+(require 'prelude-common-lisp)
+(require 'prelude-emacs-lisp)
+(require 'prelude-erc)
+(require 'prelude-js)
+(require 'prelude-latex)
+(require 'prelude-markdown)
+(require 'prelude-org)
+(require 'prelude-ruby)
+
+;; load the personal settings (this includes `custom-file')
+(when (file-exists-p prelude-personal-dir)
+  (mapc 'load (directory-files prelude-personal-dir nil "^[^#].*el$")))
+
+
+(require 'package)
+(setq package-archives (cons '("tromey" . "http://tromey.com/elpa/") package-archives))
+(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+
+(setq rsense-home "/Users/netmask/.emacs.d/rsense-0.3")
+(add-to-list 'load-path (concat rsense-home "/etc"))
+(require 'rsense)
+
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (add-to-list 'ac-sources 'ac-source-rsense-method)
+            (add-to-list 'ac-sources 'ac-source-rsense-constant)))
+
+(setq redisplay-dont-pause t)
+
+(when (require 'lusty-explorer nil 'noerror)
+
+  ;; overrride the normal file-opening, buffer switching
+  (global-set-key (kbd "C-x C-f") 'lusty-file-explorer)
+  (global-set-key (kbd "C-x b")   'lusty-buffer-explorer))
+
+(message "%s" "Emacs Prelude is ready to do thy bidding, Master!")
